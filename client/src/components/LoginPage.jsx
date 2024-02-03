ad8ed8deb4a0a81947c61918fc5332156e843ac7
Vivek Singh <viveksingh81820@gmail.com>
2024-02-03 16:27:33 +0530

    Update index.css and LoginPage.jsx

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -4,11 +4,11 @@ import gc_logo from "../assets/gc_logo.png";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Google from './../UI/googleauth'
 import { Link } from 'react-router-dom';
-
+import LoginPageStyle from './LoginPageStyle.module.css'
 const LoginPage = () => {
   return (
-    <div>
-  <section className="bg-clr flex">
+    <div className={LoginPageStyle.container}>
+  <section  className="bg-clr flex containerLogin">
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen md:h-screen lg:py-0">
       <a
         href="#"
@@ -19,10 +19,10 @@ const LoginPage = () => {
           src={gc_logo}
           alt="logo"
         />
-        <p className="text-5xl my-font">General Championship 2024</p>
+        <p className="text-5px">General Championship 2024</p>
       </a>
 
-      <div className="rounded-lg shadow border md:mt-6 lg:mt-8 xl:p-0 bg-blend-darken border-green-400 border-2 px-5 py-4 hover:text-green-600">
+      <div className="rounded-lg   md:mt-6 lg:mt-8 xl:p-0 bg-blend-darken   px-5 py-4 hover:text-green-600">
         <a
           href="#"
           className="my-font text-lg font-bold leading-tight tracking-tight md:text-2xl dark:text-green-400 hover:text-green-600 hover:border-green-600"
